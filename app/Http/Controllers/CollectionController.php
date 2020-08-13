3461d5c66fd4bc912e20f61d8913dfd825c03f96
Simon <simon.k.lonnestad@gmail.com>
2020-08-14 01:02:56 +0200

    Loot update logic
    Sum loot amounts

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -109,7 +109,32 @@ class CollectionController extends Controller
 				$collectionLog = $this->getUserCollectionLog($collection->collection_type, $user->id);
 
 				if ($collectionLog) {
-					$collectionLog->update($request->all());
+					$oldValues = $collectionLog->getAttributes(); // Get old data
+					//array_splice($oldValues, count($oldValues) - 2, 2); // Remove created_at and updated_at
+
+					$newValues = $request->all();
+
+					$sums = [];
+
+					$sums["kill_count"] = $oldValues["kill_count"] + 1;
+
+					$uniques = $oldValues["obtained"];
+
+					// Merge old data and new data and sum the total of common keys
+					foreach (array_keys($newValues + $oldValues) as $lootType) {
+						if (isset($newValues[$lootType]) && isset($oldValues[$lootType])) {
+							// If unique loot is detected, increase the total amount of uniques obtained by 1
+							if ($oldValues[$lootType] == 0) {
+								$uniques++;
+							}
+
+							$sums[$lootType] = (isset($newValues[$lootType]) ? $newValues[$lootType] : 0) + (isset($oldValues) ? $oldValues[$lootType] : 0);
+						}
+					}
+
+					$sums["obtained"] = $uniques;
+
+					$collectionLog->update($sums);
 
 					return response()->json($collectionLog, 201);
 				} else {
