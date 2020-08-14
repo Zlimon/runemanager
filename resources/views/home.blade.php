ec3fbeac58a1e42308be0e6555cb6f5ad93fe59e
Simon <simon.k.lonnestad@gmail.com>
2020-08-15 00:35:51 +0200

    Laravel Passport initial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,9 @@
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                <passport-clients></passport-clients>
+                <passport-authorized-clients></passport-authorized-clients>
+                <passport-personal-access-tokens></passport-personal-access-tokens>
             </div>
         </div>
     </div>
