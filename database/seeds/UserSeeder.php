6e411a7e9f643ffcd770eaba92f903fd29fa6efb
Zlimon <simon.k.lonnestad@gmail.com>
2021-01-10 00:47:24 +0100

    Base AccountSeed on account list

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -72,8 +72,8 @@ class UserSeeder extends Seeder
 
         shuffle($accounts);
 
-        factory(App\User::class, 35)->create()->each(function ($u) use ($accounts) {
-            $randomId = rand(0, sizeof($accounts) - 1);
+        factory(App\User::class, sizeof($accounts))->create()->each(function ($u) use ($accounts) {
+            $randomId = rand(1, sizeof($accounts));
 
             if (App\Account::where('username', $accounts[$randomId])->first()) {
                 return null;
@@ -85,86 +85,90 @@ class UserSeeder extends Seeder
             /* Get the $playerDataUrl file content. */
             $playerData = Helper::getPlayerData($playerDataUrl);
 
-            $account = Account::firstOrCreate([
-                'user_id' => rand(1, 10),
-                'account_type' => Helper::listAccountTypes()[rand(0, 3)],
-                'username' => $accounts[$randomId],
-                'rank' => $playerData[0][0],
-                'level' => $playerData[0][1],
-                'xp' => $playerData[0][2]
-            ]);
-
-            $skills = Helper::listSkills();
-
-            for ($i = 0; $i < count($skills); $i++) {
-                DB::table($skills[$i])->insert([
-                    'account_id' => $account->id,
-                    'rank' => ($playerData[$i + 1][0] >= 1 ? $playerData[$i + 1][0] : 0),
-                    'level' => $playerData[$i + 1][1],
-                    'xp' => ($playerData[$i + 1][2] >= 0 ? $playerData[$i + 1][2] : 0),
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now()
+            if ($playerData) {
+                $account = Account::firstOrCreate([
+                    'user_id' => rand(1, sizeof($accounts) - 1),
+                    'account_type' => Helper::listAccountTypes()[rand(0, 3)],
+                    'username' => $accounts[$randomId],
+                    'rank' => $playerData[0][0],
+                    'level' => $playerData[0][1],
+                    'xp' => $playerData[0][2]
                 ]);
-            }
 
-            $clueScrollAmount = count(Helper::listClueScrollTiers());
+                $skills = Helper::listSkills();
 
-            $bosses = Helper::listBosses();
-
-            array_splice($bosses, 13, 1);
-
-            $bossIndex = 0;
-
-            $dksKillCount = 0;
-
-            for ($i = (count($skills) + $clueScrollAmount + 4); $i < (count($skills) + $clueScrollAmount + 4 + count($bosses)); $i++) {
-                $collection = Collection::where('name', $bosses[$bossIndex])->firstOrFail();
-
-                $collectionLoot = new $collection->model;
-
-                $collectionLoot->account_id = $account->id;
-                $collectionLoot->kill_count = ($playerData[$i + 1][1] >= 0 ? $playerData[$i + 1][1] : 0);
-                $collectionLoot->rank = ($playerData[$i + 1][0] >= 0 ? $playerData[$i + 1][0] : 0);
-
-                if (in_array($bosses[$bossIndex],
-                    ['dagannoth prime', 'dagannoth rex', 'dagannoth supreme'], true)) {
-                    $dksKillCount += ($playerData[$i + 1][1] >= 0 ? $playerData[$i + 1][1] : 0);
+                for ($i = 0; $i < count($skills); $i++) {
+                    DB::table($skills[$i])->insert([
+                        'account_id' => $account->id,
+                        'rank' => ($playerData[$i + 1][0] >= 1 ? $playerData[$i + 1][0] : 0),
+                        'level' => $playerData[$i + 1][1],
+                        'xp' => ($playerData[$i + 1][2] >= 0 ? $playerData[$i + 1][2] : 0),
+                        'created_at' => Carbon::now(),
+                        'updated_at' => Carbon::now()
+                    ]);
                 }
 
-                $collectionLoot->save();
+                $clueScrollAmount = count(Helper::listClueScrollTiers());
 
-                $bossIndex++;
+                $bosses = Helper::listBosses();
+
+                array_splice($bosses, 13, 1);
+
+                $bossIndex = 0;
+
+                $dksKillCount = 0;
+
+                for ($i = (count($skills) + $clueScrollAmount + 4); $i < (count($skills) + $clueScrollAmount + 4 + count($bosses)); $i++) {
+                    $collection = Collection::where('name', $bosses[$bossIndex])->firstOrFail();
+
+                    $collectionLoot = new $collection->model;
+
+                    $collectionLoot->account_id = $account->id;
+                    $collectionLoot->kill_count = ($playerData[$i + 1][1] >= 0 ? $playerData[$i + 1][1] : 0);
+                    $collectionLoot->rank = ($playerData[$i + 1][0] >= 0 ? $playerData[$i + 1][0] : 0);
+
+                    if (in_array($bosses[$bossIndex],
+                        ['dagannoth prime', 'dagannoth rex', 'dagannoth supreme'], true)) {
+                        $dksKillCount += ($playerData[$i + 1][1] >= 0 ? $playerData[$i + 1][1] : 0);
+                    }
+
+                    $collectionLoot->save();
+
+                    $bossIndex++;
+                }
+
+                /**
+                 * Since there are no official total kill count hiscore for
+                 * DKS' and we are going to retrieve loot for them from the
+                 * collection log, we have to manually create a table.
+                 * This might also happen with other bosses in the future
+                 * that share collection log entry, but have separate hiscores.
+                 */
+                $dks = new \App\Boss\DagannothKings;
+
+                $dks->account_id = $account->id;
+                $dks->kill_count = $dksKillCount;
+
+                $dks->save();
+
+                $npcs = Helper::listNpcs();
+
+                foreach ($npcs as $npc) {
+                    $collection = Collection::findByNameAndCategory($npc, 4);
+
+                    $collectionLoot = new $collection->model;
+
+                    $collectionLoot->account_id = $account->id;
+
+                    $collectionLoot->save();
+                }
+
+                print_r('Added ' . $accounts[$randomId]);
+
+                return $account->toArray();
+            } else {
+                return null;
             }
-
-            /**
-             * Since there are no official total kill count hiscore for
-             * DKS' and we are going to retrieve loot for them from the
-             * collection log, we have to manually create a table.
-             * This might also happen with other bosses in the future
-             * that share collection log entry, but have separate hiscores.
-             */
-            $dks = new \App\Boss\DagannothKings;
-
-            $dks->account_id = $account->id;
-            $dks->kill_count = $dksKillCount;
-
-            $dks->save();
-
-            $npcs = Helper::listNpcs();
-
-            foreach ($npcs as $npc) {
-                $collection = Collection::findByNameAndCategory($npc, 4);
-
-                $collectionLoot = new $collection->model;
-
-                $collectionLoot->account_id = $account->id;
-
-                $collectionLoot->save();
-            }
-
-            print_r('Added ' . $accounts[$randomId]);
-
-            return $account->toArray();
         });
     }
 }
