2cf35f621de9c56ea8d87ff57254aadca1ccbf50
Zlimon <simon.k.lonnestad@gmail.com>
2021-01-12 23:30:15 +0100

    Fetch npc loot and hiscore icons

--- app/Console/Commands/NpcCreate.php
+++ app/Console/Commands/NpcCreate.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Collection;
 use Artisan;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
 class NpcCreate extends Command
@@ -42,17 +43,24 @@ class NpcCreate extends Command
     {
         $modelName = Str::studly(Str::singular(class_basename($this->argument('npc'))));
 
+        Collection::create([
+            'category_id' => 4,
+            'name' => str_replace("_", " ", Str::snake(Str::singular($this->argument('npc')))),
+            'alias' => ucfirst(str_replace("_", " ", Str::snake(Str::singular($this->argument('npc'))))),
+            'model' => "App\Npc\\" . $modelName,
+        ]);
+
         $uniques = implode(
             ' ',
             array_map(
-                function($unique) {
-                    return(str_replace("-","_", Str::snake($unique)) . ':integer:default(0):unsigned,');
+                function ($unique) {
+                    return (str_replace("-", "_", Str::snake($unique)) . ':integer:default(0):unsigned,');
                 },
                 $this->argument('unique')
             )
         );
 
-        $command = 'make:migration:schema create_' . Str::snake(Str::singular($this->argument('npc'))) . '_table --schema="account_id:integer:unsigned:unique, kill_count:integer:default(0):unsigned, obtained:integer:default(0):unsigned, ' .substr($uniques, 0, -1) . '"';
+        $command = 'make:migration:schema create_' . Str::snake(Str::singular($this->argument('npc'))) . '_table --schema="account_id:integer:unsigned:unique, kill_count:integer:default(0):unsigned, obtained:integer:default(0):unsigned, ' . substr($uniques, 0, -1) . '"';
 
         $execute = Artisan::call($command);
 
@@ -92,11 +100,44 @@ class NpcCreate extends Command
 
         Artisan::call('migrate');
 
-        Collection::create([
-            'category_id' => 4,
-            'name' => str_replace("_"," ", Str::snake(Str::singular($this->argument('npc')))),
-            'alias' => ucfirst(str_replace("_"," ", Str::snake(Str::singular($this->argument('npc'))))),
-            'model' => "App\Npc\\" . $modelName,
-        ]);
+        $i = 0;
+        foreach ($this->argument('unique') as $key => $unique) {
+            $handle = curl_init("https://api.osrsbox.com/items?where=" . urlencode('{"name":"' . ucfirst(str_replace("_", " ", Str::snake($unique))) . '","duplicate":false}'));
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+            /* Get the content of $url. */
+            $response = curl_exec($handle);
+
+            /* Check for errors (content not found). */
+            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            curl_close($handle);
+
+            /* If the document has loaded successfully without any redirection or error */
+            if ($httpCode >= 200 && $httpCode < 300) {
+                $json = json_decode($response, true);
+
+                print_r($json["_items"][0]["id"]);
+
+                $url = 'https://www.osrsbox.com/osrsbox-db/items-icons/' . (int)$json["_items"][0]["id"] . '.png'; // 4151
+
+                $dir = public_path() . '/images/npc/' . lcfirst(Str::snake(Str::singular($this->argument('npc')))) . '/'; // /images/npc/abyssal_demon/
+                $imgName = lcfirst(str_replace(" ", "_", Str::snake($unique))) . '.png'; // abyssal_whip.png
+
+                if (!File::exists($dir)) {
+                    File::makeDirectory($dir, 0777, true);
+                }
+
+                $img = $dir . $imgName;
+
+                file_put_contents($img, file_get_contents($url));
+
+                // If first iteration, make an icon for boss. This will be the first item in uniques
+                if ($i === 0) {
+                    File::copy($img, public_path() . '/images/npc/' . lcfirst(Str::snake(Str::singular($this->argument('npc')))) . '.png');
+                }
+
+                $i++;
+            }
+        }
     }
 }
