a12115a2bd81c03cb8465c3138c3e6f25a566b11
Zlimon <simon.k.lonnestad@gmail.com>
2021-01-16 17:17:21 +0100

    Account profile page redesign
    - Inspired by Adventurer's log from 2010

--- resources/views/account/show.blade.php
+++ resources/views/account/show.blade.php
@@ -7,14 +7,10 @@
 @section('content')
     <div class="col-md-12 bg-dark text-light background-dialog-panel py-3 mb-3">
         @if ($account->user->private === 0 || (Auth::check() && $account->user->id == Auth::user()->id))
-            <div class="row">
-                <div class="col-md-8">
-                    <accounthiscore account="{{ $account->username }}"></accounthiscore>
-                </div>
+            <accounthiscore account="{{ $account->username }}"></accounthiscore>
 
-                <div class="col-md-4">
-                    <accountnotification :account="{{ $account }}"></accountnotification>
-                </div>
+            <div class="col-md-6">
+                <accountnotification :account="{{ $account }}"></accountnotification>
             </div>
         @else
             <div class="text-center py-5">
