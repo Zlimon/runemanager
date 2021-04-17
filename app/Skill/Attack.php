db527dd836ddb01aecefd720ea9665c5b81fed6d
Zlimon <simon.k.lonnestad@gmail.com>
2021-04-18 00:16:49 +0200

    Skill models and migrations

---
+++ app/Skill/Attack.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Skill;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attack extends Model
+{
+    use HasFactory;
+}
---
+++ app/Skill/Slayer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Skill;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Slayer extends Model
+{
+    use HasFactory;
+}
