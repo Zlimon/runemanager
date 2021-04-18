009e28603e8e39165f5a289a84799a37649e76c4
Zlimon <simon.k.lonnestad@gmail.com>
2021-04-18 22:31:49 +0200

    Correct total level icon

--- resources/js/components/AccountSkillHiscore.vue
+++ resources/js/components/AccountSkillHiscore.vue
@@ -31,8 +31,8 @@
                     </div>
                     <div class="button-combat-style-narrow text-center button-small">
                         <a :href="'/hiscore/skill/total'">
-                            <img :alt="'Total level skill icon'"
-                                 :src="'/images/skill/total.png'"
+                            <img :alt="'Total level icon'"
+                                 :src="'/storage/resource-pack/skill/total.png'"
                                  :title="'Click here to visit total level hiscores'"
                                  class="">
                             <span>{{ data.level }}</span>
