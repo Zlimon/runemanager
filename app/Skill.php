dfae86b5a6bbcbc388e4b8d07f772297243c6940
Zlimon <simon.k.lonnestad@gmail.com>
2021-04-24 14:45:18 +0200

    Skill model cleanup

--- app/Skill.php
+++ app/Skill.php
@@ -6,22 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Skill extends Model
 {
-    protected $fillable = ['category_id', 'name', 'alias', 'model'];
-
     public $timestamps = false;
-
-    public static function findByNameAndCategory($name, $category_id)
-    {
-        return self::where([['name', $name], ['category_id', $category_id]])->first();
-    }
-
-    public function category()
-    {
-        return $this->hasOne(Category::class);
-    }
-
-    public function collection()
-    {
-        return $this->morphTo();
-    }
 }
