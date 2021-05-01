5740e0e8167590d220f5bb7e8430f97f8ddbaca0
Zlimon <simon.k.lonnestad@gmail.com>
2021-05-01 20:13:28 +0200

    Skill table migration corrections

--- database/migrations/2021_04_17_220819_create_skills_table.php
+++ database/migrations/2021_04_17_220819_create_skills_table.php
@@ -17,173 +17,173 @@ class CreateSkillsTable extends Migration
             $table->id();
             $table->integer('category_id')->unsigned();
             $table->integer('order')->unsigned()->unique();
-            $table->string('name');
-            $table->string('alias')->unique();
+            $table->string('name')->unique();
+            $table->string('slug');
             $table->string('model');
         });
 
-        DB::table('skills')->insert(
+        DB::table("skills")->insert(
             [
                 [
-                    "category_id" => 1,
-                    "order" => 1,
-                    "name" => "attack",
-                    "alias" => "Attack",
-                    "model" => "App\Skill\Attack"
+                    'category_id' => 1,
+                    'order' => 1,
+                    'name' => 'Attack',
+                    'slug' => 'attack',
+                    'model' => 'App\Skill\Attack'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 2,
-                    "name" => "defence",
-                    "alias" => "Defence",
-                    "model" => "App\Skill\Defence"
+                    'category_id' => 1,
+                    'order' => 2,
+                    'name' => 'Defence',
+                    'slug' => 'defence',
+                    'model' => 'App\Skill\Defence'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 3,
-                    "name" => "strength",
-                    "alias" => "Strength",
-                    "model" => "App\Skill\Strength"
+                    'category_id' => 1,
+                    'order' => 3,
+                    'name' => 'Strength',
+                    'slug' => 'strength',
+                    'model' => 'App\Skill\Strength'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 4,
-                    "name" => "hitpoints",
-                    "alias" => "Hitpoints",
-                    "model" => "App\Skill\Hitpoints"
+                    'category_id' => 1,
+                    'order' => 4,
+                    'name' => 'Hitpoints',
+                    'slug' => 'hitpoints',
+                    'model' => 'App\Skill\Hitpoints'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 5,
-                    "name" => "ranged",
-                    "alias" => "Ranged",
-                    "model" => "App\Skill\Ranged"
+                    'category_id' => 1,
+                    'order' => 5,
+                    'name' => 'Ranged',
+                    'slug' => 'ranged',
+                    'model' => 'App\Skill\Ranged'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 6,
-                    "name" => "prayer",
-                    "alias" => "Prayer",
-                    "model" => "App\Skill\Prayer"
+                    'category_id' => 1,
+                    'order' => 6,
+                    'name' => 'Prayer',
+                    'slug' => 'prayer',
+                    'model' => 'App\Skill\Prayer'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 7,
-                    "name" => "magic",
-                    "alias" => "Magic",
-                    "model" => "App\Skill\Magic"
+                    'category_id' => 1,
+                    'order' => 7,
+                    'name' => 'Magic',
+                    'slug' => 'magic',
+                    'model' => 'App\Skill\Magic'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 8,
-                    "name" => "cooking",
-                    "alias" => "Cooking",
-                    "model" => "App\Skill\Cooking"
+                    'category_id' => 1,
+                    'order' => 8,
+                    'name' => 'Cooking',
+                    'slug' => 'cooking',
+                    'model' => 'App\Skill\Cooking'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 9,
-                    "name" => "woodcutting",
-                    "alias" => "Woodcutting",
-                    "model" => "App\Skill\Woodcutting"
+                    'category_id' => 1,
+                    'order' => 9,
+                    'name' => 'Woodcutting',
+                    'slug' => 'woodcutting',
+                    'model' => 'App\Skill\Woodcutting'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 10,
-                    "name" => "fletching",
-                    "alias" => "Fletching",
-                    "model" => "App\Skill\Fletching"
+                    'category_id' => 1,
+                    'order' => 10,
+                    'name' => 'Fletching',
+                    'slug' => 'fletching',
+                    'model' => 'App\Skill\Fletching'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 11,
-                    "name" => "fishing",
-                    "alias" => "Fishing",
-                    "model" => "App\Skill\Fishing"
+                    'category_id' => 1,
+                    'order' => 11,
+                    'name' => 'Fishing',
+                    'slug' => 'fishing',
+                    'model' => 'App\Skill\Fishing'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 12,
-                    "name" => "firemaking",
-                    "alias" => "Firemaking",
-                    "model" => "App\Skill\Firemaking"
+                    'category_id' => 1,
+                    'order' => 12,
+                    'name' => 'Firemaking',
+                    'slug' => 'firemaking',
+                    'model' => 'App\Skill\Firemaking'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 13,
-                    "name" => "crafting",
-                    "alias" => "Crafting",
-                    "model" => "App\Skill\Crafting"
+                    'category_id' => 1,
+                    'order' => 13,
+                    'name' => 'Crafting',
+                    'slug' => 'crafting',
+                    'model' => 'App\Skill\Crafting'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 14,
-                    "name" => "smithing",
-                    "alias" => "Smithing",
-                    "model" => "App\Skill\Smithing"
+                    'category_id' => 1,
+                    'order' => 14,
+                    'name' => 'Smithing',
+                    'slug' => 'smithing',
+                    'model' => 'App\Skill\Smithing'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 15,
-                    "name" => "mining",
-                    "alias" => "Mining",
-                    "model" => "App\Skill\Mining"
+                    'category_id' => 1,
+                    'order' => 15,
+                    'name' => 'Mining',
+                    'slug' => 'mining',
+                    'model' => 'App\Skill\Mining'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 16,
-                    "name" => "herblore",
-                    "alias" => "Herblore",
-                    "model" => "App\Skill\Herblore"
+                    'category_id' => 1,
+                    'order' => 16,
+                    'name' => 'Herblore',
+                    'slug' => 'herblore',
+                    'model' => 'App\Skill\Herblore'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 17,
-                    "name" => "agility",
-                    "alias" => "Agility",
-                    "model" => "App\Skill\Agility"
+                    'category_id' => 1,
+                    'order' => 17,
+                    'name' => 'Agility',
+                    'slug' => 'agility',
+                    'model' => 'App\Skill\Agility'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 18,
-                    "name" => "thieving",
-                    "alias" => "Thieving",
-                    "model" => "App\Skill\Thieving"
+                    'category_id' => 1,
+                    'order' => 18,
+                    'name' => 'Thieving',
+                    'slug' => 'thieving',
+                    'model' => 'App\Skill\Thieving'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 19,
-                    "name" => "slayer",
-                    "alias" => "Slayer",
-                    "model" => "App\Skill\Slayer"
+                    'category_id' => 1,
+                    'order' => 19,
+                    'name' => 'Slayer',
+                    'slug' => 'slayer',
+                    'model' => 'App\Skill\Slayer'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 20,
-                    "name" => "farming",
-                    "alias" => "Farming",
-                    "model" => "App\Skill\Farming"
+                    'category_id' => 1,
+                    'order' => 20,
+                    'name' => 'Farming',
+                    'slug' => 'farming',
+                    'model' => 'App\Skill\Farming'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 21,
-                    "name" => "runecraft",
-                    "alias" => "Runecraft",
-                    "model" => "App\Skill\Runecraft"
+                    'category_id' => 1,
+                    'order' => 21,
+                    'name' => 'Runecraft',
+                    'slug' => 'runecraft',
+                    'model' => 'App\Skill\Runecraft'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 22,
-                    "name" => "hunter",
-                    "alias" => "Hunter",
-                    "model" => "App\Skill\Hunter"
+                    'category_id' => 1,
+                    'order' => 22,
+                    'name' => 'Hunter',
+                    'slug' => 'hunter',
+                    'model' => 'App\Skill\Hunter'
                 ],
                 [
-                    "category_id" => 1,
-                    "order" => 23,
-                    "name" => "construction",
-                    "alias" => "Construction",
-                    "model" => "App\Skill\Construction"
+                    'category_id' => 1,
+                    'order' => 23,
+                    'name' => 'Construction',
+                    'slug' => 'construction',
+                    'model' => 'App\Skill\Construction'
                 ]
             ]
         );
