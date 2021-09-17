82dd2fe8fa7f7f2e32e58fd524c357f63569b72c
Zlimon <simon.k.lonnestad@gmail.com>
2021-09-17 20:04:20 +0200

    Bootstrap 5 - Admin Dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,63 +5,92 @@
 @endsection
 
 @section('content')
-    <div style="color: black;">
-        <div class="card-deck">
-            <div class="card text-center">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="col">
+            <div class="card h-100 bg-admin-secondary">
                 <div class="card-body">
                     <div class="float-left"><i class="fas fa-users" style="font-size: 4em; font-weight: 900;"></i></div>
                     <h5 class="card-title">Total users</h5>
                     <p class="card-text"><strong>{{ $users }}</strong></p>
                 </div>
             </div>
-
-            <div class="card text-center">
+        </div>
+        <div class="col">
+            <div class="card h-100 bg-admin-secondary">
                 <div class="card-body">
                     <div class="float-left"><i class="fas fa-users" style="font-size: 4em; font-weight: 900;"></i></div>
-                    <h5 class="card-title">Lorem ipsum</h5>
-                    <p class="card-text"><strong>dolor sit amet</strong></p>
-                </div>
-            </div>
-
-            <div class="card text-center">
-                <div class="card-body">
-                    <div class="float-left"><i class="fas fa-users" style="font-size: 4em; font-weight: 900;"></i></div>
-                    <h5 class="card-title">Lorem ipsum</h5>
-                    <p class="card-text"><strong>dolor sit amet</strong></p>
-                </div>
-            </div>
-
-            <div class="card text-center">
-                <div class="card-body">
-                    <div class="float-left"><i class="fas fa-users" style="font-size: 4em; font-weight: 900;"></i></div>
-                    <h5 class="card-title">Lorem ipsum</h5>
-                    <p class="card-text"><strong>dolor sit amet</strong></p>
+                    <h5 class="card-title">Total users</h5>
+                    <p class="card-text"><strong>{{ $users }}</strong></p>
                 </div>
             </div>
         </div>
-
-        <div class="card-deck mt-5">
-            @for ($i = 0; $i < 2; $i++)
-                <div class="card border-0">
-                    <div class="card-header text-light" style="background: #6d7fcc;">Tittel</div>
-                    <div class="card-body">
-                        <h5 class="card-title">Lorem ipsum</h5>
-                        <p class="card-text"><strong>dolor sit amet</strong></p>
-                    </div>
+        <div class="col">
+            <div class="card h-100 bg-admin-secondary">
+                <div class="card-body">
+                    <div class="float-left"><i class="fas fa-users" style="font-size: 4em; font-weight: 900;"></i></div>
+                    <h5 class="card-title">Total users</h5>
+                    <p class="card-text"><strong>{{ $users }}</strong></p>
                 </div>
-            @endfor
+            </div>
         </div>
-
-        <div class="card-deck mt-5">
-            @for ($i = 0; $i < 2; $i++)
-                <div class="card border-0">
-                    <div class="card-header text-light" style="background: #6d7fcc;">Tittel</div>
-                    <div class="card-body">
-                        <h5 class="card-title">Lorem ipsum</h5>
-                        <p class="card-text"><strong>dolor sit amet</strong></p>
-                    </div>
+        <div class="col">
+            <div class="card h-100 bg-admin-secondary">
+                <div class="card-body">
+                    <div class="float-left"><i class="fas fa-users" style="font-size: 4em; font-weight: 900;"></i></div>
+                    <h5 class="card-title">Total users</h5>
+                    <p class="card-text"><strong>{{ $users }}</strong></p>
                 </div>
-            @endfor
+            </div>
+        </div>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-2 g-4 mt-3">
+        <div class="col">
+            <div class="card h-100 bg-admin-secondary border-0">
+                <div class="card-header bg-admin-info">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 bg-admin-secondary border-0">
+                <div class="card-header bg-admin-info">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a short card.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 bg-admin-secondary border-0">
+                <div class="card-header bg-admin-info">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                        additional content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 bg-admin-secondary border-0">
+                <div class="card-header bg-admin-info">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
