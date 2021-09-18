665006cbe28a7d92c179ab12045f22081748988b
Zlimon <simon.k.lonnestad@gmail.com>
2021-09-19 00:32:12 +0200

    Better responsive layout & Improved description

--- resources/views/admin/account/create.blade.php
+++ resources/views/admin/account/create.blade.php
@@ -9,43 +9,49 @@
 @endsection
 
 @section('content')
-    <h1>Register an OSRS account</h1>
-    <p>If you want to preregister an OSRS account for future use / reference, you can do it here.</p>
-    <p>This means this account will not be able to be linked for a new user, but have to be linked manually.</p>
+    <div class="row">
+        <div class="col-12 col-md-4">
+            <div class="bg-admin-dark p-4">
+                <h1>Reserve an OSRS account</h1>
+                <p>If you want to reserve an Old School RuneScape account for future use / prevent it being claimed, you can do it here.</p>
+                <p>To link the account to an user, visit the account page and fill the "Transfer ownership of this account" form.</p>
 
-    <div class="bg-admin-dark p-4">
-        <form method="POST" action="{{ route('admin-create-account') }}">
-            @csrf
+                <div class="p-4 mb-4 bg-admin-info">
+                    <form method="POST" action="{{ route('admin-create-account') }}">
+                        @csrf
 
-            <div class="mb-3">
-                <label for="account" class="form-label">Old School RuneScape username</label>
-                <input type="text"
-                       id="account"
-                       name="account"
-                       class="form-control @error('account') is-invalid @enderror"
-                       autofocus required>
+                        <div class="mb-3">
+                            <label for="account" class="form-label">Old School RuneScape username</label>
+                            <input type="text"
+                                   id="account"
+                                   name="account"
+                                   class="form-control @error('account') is-invalid @enderror"
+                                   autofocus required>
+                        </div>
+                        @error('account')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+
+                        <div class="mb-3">
+                            <label for="account" class="form-label">RuneManager username or ID</label>
+                            <input type="text"
+                                   id="user"
+                                   name="user"
+                                   class="form-control @error('account') is-invalid @enderror"
+                                   required>
+                        </div>
+                        @error('user')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+
+                        <button type="submit" class="btn btn-primary">Register</button>
+                    </form>
+                </div>
             </div>
-            @error('account')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-
-            <div class="mb-3">
-                <label for="account" class="form-label">RuneManager user username or ID</label>
-                <input type="text"
-                       id="user"
-                       name="user"
-                       class="form-control @error('account') is-invalid @enderror"
-                       required>
-            </div>
-            @error('user')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-
-            <button type="submit" class="btn btn-primary">Register</button>
-        </form>
+        </div>
     </div>
 @endsection
