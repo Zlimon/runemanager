eb855337d7e14b469b4a7cfdec65a1ffd0c26c26
Zlimon <simon.k.lonnestad@gmail.com>
2021-09-18 20:36:44 +0200

    Bootstrap 5 - Admin Account Create

--- resources/views/admin/account/create.blade.php
+++ resources/views/admin/account/create.blade.php
@@ -13,33 +13,39 @@
     <p>If you want to preregister an OSRS account for future use / reference, you can do it here.</p>
     <p>This means this account will not be able to be linked for a new user, but have to be linked manually.</p>
 
-    <form method="POST" action="{{ route('admin-create-account') }}">
-        @csrf
+    <div class="bg-admin-dark p-4">
+        <form method="POST" action="{{ route('admin-create-account') }}">
+            @csrf
 
-        <label for="account" class="col-form-label text-md-right">RuneScape username</label>
+            <div class="mb-3">
+                <label for="account" class="form-label">Old School RuneScape username</label>
+                <input type="text"
+                       id="account"
+                       name="account"
+                       class="form-control @error('account') is-invalid @enderror"
+                       autofocus required>
+            </div>
+            @error('account')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 
-        <input id="account" type="text" class="form-control col-3 @error('account') is-invalid @enderror"
-               name="account" value="{{ old('account') }}" required autofocus>
+            <div class="mb-3">
+                <label for="account" class="form-label">RuneManager user username or ID</label>
+                <input type="text"
+                       id="user"
+                       name="user"
+                       class="form-control @error('account') is-invalid @enderror"
+                       required>
+            </div>
+            @error('user')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 
-        @error('account')
-        <span class="invalid-feedback" role="alert">
-				<strong>{{ $message }}</strong>
-			</span>
-        @enderror
-
-        <label for="user" class="col-form-label text-md-right">RuneManager user username or ID</label>
-
-        <input id="user" type="text" class="form-control col-3 @error('user') is-invalid @enderror"
-               name="user" value="{{ old('user') }}">
-
-        @error('user')
-        <span class="invalid-feedback" role="alert">
-				<strong>{{ $message }}</strong>
-			</span>
-        @enderror
-
-        <div class="mt-2">
             <button type="submit" class="btn btn-primary">Register</button>
-        </div>
-    </form>
+        </form>
+    </div>
 @endsection
