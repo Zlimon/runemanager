68e936d635b3a0325f2cfe1682e136b76dedbb2b
Zlimon <simon.k.lonnestad@gmail.com>
2021-09-19 20:49:47 +0200

    Bootstrap 5 - Settings

--- resources/views/admin/setting/index.blade.php
+++ resources/views/admin/setting/index.blade.php
@@ -9,29 +9,34 @@
 @endsection
 
 @section('content')
-    @if (count(config('settings', [])) )
-        <form method="POST" action="{{ route('admin-settings-store') }}">
-            @csrf
+    <div class="row">
+        <div class="col">
+            <div class="p-4 bg-admin-dark">
+                @if (count(config('settings', [])) )
+                    <form method="POST" action="{{ route('admin-settings-store') }}">
+                        @csrf
 
-            <div class="row flex-wrap">
-                @foreach(config('settings') as $section => $fields)
-                    <div class="col-12 col-md-6">
-                    <h2><i class="{{ $fields['icon'] }}"></i> {{ $fields['title'] }}</h2>
+                        <div class="row flex-wrap">
+                            @foreach(config('settings') as $section => $fields)
+                                <div class="col-12 col-md-6">
+                                    <h2><i class="{{ $fields['icon'] }}"></i> {{ $fields['title'] }}</h2>
+                                    <p>{{ $fields['desc'] }}</p>
 
-                    <p class="text-muted">{{ $fields['desc'] }}</p>
+                                    <div class="p-4 mb-4 bg-admin-info">
+                                        @foreach($fields['elements'] as $field)
+                                            @includeIf('admin.setting.fields.' . $field['type'] )
+                                        @endforeach
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
 
-                    @foreach($fields['elements'] as $field)
-                        @includeIf('admin.setting.fields.' . $field['type'] )
-                    @endforeach
-
-                    <hr>
-                    </div>
-                @endforeach
+                        <div class="mt-2">
+                            <button type="submit" class="btn btn-primary">Save</button>
+                        </div>
+                    </form>
+                @endif
             </div>
-
-            <div class="mt-2">
-                <button type="submit" class="btn btn-primary">Save</button>
-            </div>
-        </form>
-    @endif
+        </div>
+    </div>
 @endsection
