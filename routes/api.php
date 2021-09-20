83f98fc349bc32e27529b54402093fae4ba8c88e
Zlimon <simon.k.lonnestad@gmail.com>
2021-09-20 22:44:40 +0200

    Separate Admin API and Web route files

--- routes/api.php
+++ routes/api.php
@@ -108,19 +108,3 @@ Route::prefix('/group')->group(function() {
     Route::get('/{group}', 'Api\GroupController@show');
 	Route::get('/{group}/bank', 'Api\GroupController@bank');
 });
-
-Route::middleware('auth:api')->group(function() {
-    Route::prefix('/admin')->group(function() {
-        Route::post('/news/create', 'Admin\Api\NewsController@store')->name('admin-store-newspost');
-        Route::put('/news/{newsPost}/update', 'Admin\Api\NewsController@update')->name('admin-update-newspost');
-
-        Route::post('/calendar/create', 'Admin\Api\CalendarController@store');
-        Route::post('/calendar/{calendar}/update', 'Admin\Api\CalendarController@update');
-        Route::patch('/calendar/{calendar}/schedule', 'Admin\Api\CalendarController@updateSchedule');
-        Route::delete('/calendar/{calendar}/destroy', 'Admin\Api\CalendarController@destroy');
-
-		Route::post('/settings/resource-pack', 'Admin\Api\ResourcePackController@search')->name('admin-settings-resourcepack-search');
-		Route::patch('/settings/resource-pack/{resourcePack}/switch', 'Admin\Api\ResourcePackController@switch')->name('admin-settings-resourcepack-switch');
-		Route::patch('/settings/resource-pack/{resourcePack}/update', 'Admin\Api\ResourcePackController@update')->name('admin-settings-resourcepack-update');
-    });
-});
--- routes/web.php
+++ routes/web.php
@@ -52,44 +52,4 @@ Route::patch('/user/edit', 'UserController@update')->name('user-update');
 Route::get('/news', 'NewsController@index')->name('news');
 Route::get('/news/{id}', 'NewsController@show')->name('news-show');
 
-/*==========Admin Controller=============*/
-//	Route::group(['middleware' => ['permission:access admin']], function () {
-Route::middleware('auth')->group(function() {
-    Route::prefix('/admin')->group(function() {
-		Route::get('/', 'Admin\AdminController@index')->name('admin-index');
-		/*==========News Controller=============*/
-		Route::get('/news', 'Admin\NewsController@index')->name('admin-news');
-		Route::get('/news/create', 'Admin\NewsController@create')->name('admin-create-newspost');
-		Route::get('/news/{newsPost}/show', 'Admin\NewsController@show')->name('admin-show-newspost');
-		Route::get('/news/{newsPost}/edit', 'Admin\NewsController@edit')->name('admin-edit-newspost');
-		Route::delete('/news/{newsPost}delete', 'Admin\NewsController@destroy')->name('admin-delete-newspost');
-
-		Route::post('/news/category/create', 'Admin\NewsController@createCategory')->name('admin-create-newspost-category');
-		//Route::resource('/admin/news', 'AdminNewsController');
-		/*==========User Controller=============*/
-		Route::get('/user', 'Admin\UserController@index')->name('admin-user');
-		Route::post('/user', 'Admin\UserController@search')->name('admin-search-user');
-		Route::get('/user/{user}/show', 'Admin\UserController@show')->name('admin-show-user');
-		Route::get('/user/{user}/edit', 'Admin\UserController@edit')->name('admin-edit-user');
-		Route::patch('/user/{user}/edit', 'Admin\UserController@update')->name('admin-update-user');
-		/*==========Member Controller=============*/
-		Route::get('/account', 'Admin\AccountController@index')->name('admin-account');
-		Route::post('/account', 'Admin\AccountController@search')->name('admin-search-account');
-		Route::get('/account/create', 'Admin\AccountController@create')->name('admin-create-account');
-		Route::post('/account/create', 'Admin\AccountController@store')->name('admin-store-account');
-		Route::get('/account/{account}/show', 'Admin\AccountController@show')->name('admin-show-account');
-		Route::patch('/account/{account}/show', 'Admin\AccountController@update')->name('admin-update-account');
-		/*==========Calendar Controller=============*/
-		Route::get('/calendar', 'Admin\CalendarController@index')->name('admin-calendar');
-		Route::delete('/calendar/truncate', 'Admin\CalendarController@truncate')->name('admin-calendar-truncate');
-		/*==========Settings Controller=============*/
-		Route::get('/settings', 'Admin\SettingsController@index')->name('admin-settings');
-		Route::post('/settings', 'Admin\SettingsController@store')->name('admin-settings-store');
-		Route::get('/settings/resource-pack', 'Admin\SettingsController@resourcePack')->name('admin-settings-resourcepack');
-		/*==========Rank Controller=============*/
-//		Route::get('/admin/rank', 'AdminRoleController@index')->name('admin-rank');
-//	});
-    });
-});
-
 Auth::routes();
