0039c121d791bc70d0554fa85b72f59800dc3dbe
Zlimon <simon.k.lonnestad@gmail.com>
2021-09-21 20:33:18 +0200

    Refactor User update

--- app/Http/Controllers/Admin/Api/UserController.php
+++ app/Http/Controllers/Admin/Api/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin\Api;
 
 use App\Http\Controllers\Controller;
+use App\Rules\ValidateIconId;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -70,74 +72,80 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        if (!Helper::verifyItem(request('icon_id'))) {
-            return redirect(route('admin-edit-user', $user))->withErrors(['Invalid icon ID']);
-        }
-
-        $user->update(request()->validate([
+        $this->validate($request, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255',
                 Rule::unique('users')->ignore($user->id),
             ],
             'private' => ['boolean'],
-            'icon_id' => ['integer'],
-        ]));
+            'icon_id' => ['integer', new ValidateIconId()],
+        ]);
 
-        $accountsList = $request->input('account');
-        $accountId = $request->input('accountId');
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->private = $request->private;
+        $user->icon_id = $request->icon_id;
 
-        $found = [];
-        $notFound = [];
+        $user->save();
 
-        foreach ($accountsList as $key => $newOwner) {
-            if (!$newOwner) continue;
+        return response($user, 202);
 
-            $account = Account::find($accountId[$key]);
 
-            if (!$account) continue;
-
-            $getNewOwner = User::where('name', $newOwner)->first();
-
-            if ($getNewOwner) {
-                $account->update([
-                    'user_id' => $getNewOwner->id
-                ]);
-
-                array_push($found, $getNewOwner->name);
-            } else {
-                $getNewOwner = User::find($newOwner);
-
-                if ($getNewOwner) {
-                    $account->update([
-                        'user_id' => $getNewOwner->id
-                    ]);
-
-                    array_push($found, $getNewOwner->name);
-                } else {
-                    array_push($notFound, $newOwner);
-                }
-            }
-        }
-
-        if (count($found) > 0) {
-            if (count($notFound) > 0) {
-                return redirect(route('admin-edit-user', $user))->with('message', ($user->wasChanged() ? 'Profile updated!' : '').'Accounts transferred to: '.implode(', ',$found))->withErrors(['Users not found: '.implode(',',$notFound)]);
-            } else {
-                return redirect(route('admin-edit-user', $user))->with('message', ($user->wasChanged() ? 'Profile updated!' : '').'Accounts transferred to: '.implode(', ',$found));
-            }
-        } elseif (count($notFound) > 0) {
-                if ($user->wasChanged()) {
-                    return redirect(route('admin-edit-user', $user))->with('message', 'Profile updated!')->withErrors(['Users not found: '.implode(',',$notFound)]);
-                } else {
-                    return redirect(route('admin-edit-user', $user))->withErrors(['Users not found: '.implode(',',$notFound)]);
-                }
-        } else {
-            if ($user->wasChanged()) {
-                return redirect(route('admin-edit-user', $user))->with('message', 'Profile updated!');
-            } else {
-                return redirect(route('admin-edit-user', $user));
-            }
-        }
+//        $accountsList = $request->input('account');
+//        $accountId = $request->input('accountId');
+//
+//        $found = [];
+//        $notFound = [];
+//
+//        foreach ($accountsList as $key => $newOwner) {
+//            if (!$newOwner) continue;
+//
+//            $account = Account::find($accountId[$key]);
+//
+//            if (!$account) continue;
+//
+//            $getNewOwner = User::where('name', $newOwner)->first();
+//
+//            if ($getNewOwner) {
+//                $account->update([
+//                    'user_id' => $getNewOwner->id
+//                ]);
+//
+//                array_push($found, $getNewOwner->name);
+//            } else {
+//                $getNewOwner = User::find($newOwner);
+//
+//                if ($getNewOwner) {
+//                    $account->update([
+//                        'user_id' => $getNewOwner->id
+//                    ]);
+//
+//                    array_push($found, $getNewOwner->name);
+//                } else {
+//                    array_push($notFound, $newOwner);
+//                }
+//            }
+//        }
+//
+//        if (count($found) > 0) {
+//            if (count($notFound) > 0) {
+//                return redirect(route('admin-edit-user', $user))->with('message', ($user->wasChanged() ? 'Profile updated!' : '').'Accounts transferred to: '.implode(', ',$found))->withErrors(['Users not found: '.implode(',',$notFound)]);
+//            } else {
+//                return redirect(route('admin-edit-user', $user))->with('message', ($user->wasChanged() ? 'Profile updated!' : '').'Accounts transferred to: '.implode(', ',$found));
+//            }
+//        } elseif (count($notFound) > 0) {
+//                if ($user->wasChanged()) {
+//                    return redirect(route('admin-edit-user', $user))->with('message', 'Profile updated!')->withErrors(['Users not found: '.implode(',',$notFound)]);
+//                } else {
+//                    return redirect(route('admin-edit-user', $user))->withErrors(['Users not found: '.implode(',',$notFound)]);
+//                }
+//        } else {
+//            if ($user->wasChanged()) {
+//                return redirect(route('admin-edit-user', $user))->with('message', 'Profile updated!');
+//            } else {
+//                return redirect(route('admin-edit-user', $user));
+//            }
+//        }
     }
 
     /**
