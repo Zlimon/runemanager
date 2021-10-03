bafffa4551402b30ef02fb8956c38f8304abc5e7
Zlimon <simon.k.lonnestad@gmail.com>
2021-10-03 21:14:04 +0200

    Bootstrap 5 - Calendar

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -5,7 +5,18 @@
 @endsection
 
 @section('content')
-    <div class="col-md-12 bg-dark text-light background-dialog-panel py-3">
-        <calendar></calendar>
+    <div class="row mb-3">
+        <div class="col-3 d-none d-md-block">
+            <div class="bg-dark background-dialog-panel p-3">
+                <h2 class="text-center header-chatbox-sword">Notifications</h2>
+                <announcementall></announcementall>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="bg-dark background-dialog-panel p-3">
+                <calendar></calendar>
+            </div>
+        </div>
     </div>
 @endsection
