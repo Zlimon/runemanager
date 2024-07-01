8880f9d758815d5b9acd4cff045a5d2427944bcc
Zlimon <simon@lonnestad.no>
2024-07-01 19:17:11 +0200

    Team functionality

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Zlimon',
+            'email' => 'zlimon@runemanager.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('test1234'),
+            'remember_token' => Str::random(10),
+            'icon_id' => Helper::randomItemId(true),
+        ]);
+    }
+}
