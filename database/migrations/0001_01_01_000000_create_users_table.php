da63b859d2368275c33e6e09b2d6d7d481e9cf62
Zlimon <simon@lonnestad.no>
2024-07-01 21:18:29 +0200

    Adjust migration columns

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('icon_id');
+            $table->boolean('private')->default(false);
             $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
 
--- database/migrations/2020_06_21_235131_add_two_factor_columns_to_users_table.php
+++ database/migrations/2020_06_21_235131_add_two_factor_columns_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->text('two_factor_secret')
-                ->after('password')
+                ->after('current_team_id')
                 ->nullable();
 
             $table->text('two_factor_recovery_codes')
