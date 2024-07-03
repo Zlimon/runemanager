fa9deec0511473bbb11de52d49f9617744f800e7
Zlimon <simon@lonnestad.no>
2024-07-03 20:43:29 +0200

    Button components

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,14 +1,26 @@
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
     type: {
         type: String,
         default: 'submit',
     },
+  disabled: Boolean,
+});
+
+const classes = computed(() => {
+    return props.disabled
+        ? 'opacity-50 cursor-not-allowed'
+        : '';
 });
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
+    <button :type="type"
+            class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150"
+            :class="classes"
+            :disabled="disabled">
         <slot />
     </button>
 </template>
--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -1,14 +1,26 @@
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
     type: {
         type: String,
         default: 'button',
     },
+  disabled: Boolean,
+});
+
+const classes = computed(() => {
+    return props.disabled
+        ? 'opacity-50 cursor-not-allowed'
+        : '';
 });
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
+    <button :type="type"
+            class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
+            :class="classes"
+            :disabled="disabled">
         <slot />
     </button>
 </template>
