88b2114fe3f7629c7155ef4c5a4ccd542be0d357
Zlimon <simon@lonnestad.no>
2024-07-04 18:37:14 +0200

    Account and User resources

---
+++ app/Http/Resources/AccountResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AccountResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'user_id' => $this->user_id,
+            'user' => (new UserResource($this->whenLoaded('user'))),
+            'account_type' => $this->account_type,
+            'username' => $this->username,
+            'rank' => $this->rank,
+            'level' => $this->level,
+            'xp' => $this->xp,
+            'online' => $this->online,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'password' => $this->password,
+            'icon_id' => $this->icon_id,
+            'private' => $this->private,
+            'current_team_id' => $this->current_team_id,
+            'two_factor_secret' => $this->two_factor_secret,
+            'two_factor_recovery_codes' => $this->two_factor_recovery_codes,
+            'two_factor_confirmed_at' => $this->two_factor_confirmed_at,
+            'remember_token' => $this->remember_token,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
