c568b78d13ba5ed580b9b8be4b375c7fe3826f65
Zlimon <simon@lonnestad.no>
2024-07-04 18:37:37 +0200

    Account model & User Account model relation

---
+++ app/Models/Account.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    public function getRouteKeyName(): string
+    {
+        return 'username';
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+//    public function log() {
+//        return $this->hasMany(Log::class);
+//    }
+//
+//    public function logBroadcast() {
+//        return $this->hasManyThrough(Broadcast::class, Log::class);
+//    }
+//
+//    public function collection(Collection $collection) {
+//        return $this->hasOne($collection->model);
+//    }
+//
+//    public function skill(Skill $skill) {
+//        return $this->hasOne($skill->model);
+//    }
+//
+//    public function equipment() {
+//        return $this->hasOne(Equipment::class);
+//    }
+//
+//    public function bank() {
+//        return $this->hasOne(Bank::class);
+//    }
+//
+//    public function quest() {
+//        return $this->hasOne(Quest::class);
+//    }
+//
+//    public function group() {
+//        return $this->belongsToMany(Group::class);
+//    }
+}
