0bceb09369c93fb47704005c2566d77399882cd0
Zlimon <simon@lonnestad.no>
2024-07-04 22:18:07 +0200

    Account search function

--- resources/js/Pages/Accounts/Index.vue
+++ resources/js/Pages/Accounts/Index.vue
@@ -1,9 +1,47 @@
 <script setup>
+import {onMounted, ref, watch} from "vue";
+import debounce from 'lodash/debounce';
 import AppLayout from '@/Layouts/AppLayout.vue';
 
-const props = defineProps({
-    accounts: Object,
+let accounts = ref([]);
+let search = ref('');
+let payload = {};
+let perPage = ref(10);
+let loading = ref(false);
+
+onMounted(() => {
+    searchAccounts(payload);
 });
+
+watch(search, debounce(function (value) {
+    value = value.replaceAll(' ', '-').toLowerCase();
+
+    payload = {
+        ...payload,
+        'search': value,
+    };
+
+    searchAccounts(payload)
+}, 500));
+
+const searchAccounts = (query, load = true) => {
+    loading.value = load;
+
+    query = {
+        ...payload,
+        'per_page': perPage.value,
+    };
+
+    axios
+        .post(route('api.accounts.search'), query)
+        .then((response) => {
+            accounts.value = response.data;
+        }).catch(error => {
+            console.error(error)
+        }).finally(() => {
+            loading.value = false;
+    });
+};
 </script>
 
 <template>
@@ -22,18 +60,23 @@ const props = defineProps({
                             Search for accounts
                         </h1>
 
-                        <form class="max-w-md mx-auto mt-6 lg:mt-6">
+                        <div class="max-w-md mx-auto mt-6 lg:mt-6">
                             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+
                             <div class="relative">
                                 <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                                     <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                     </svg>
                                 </div>
-                                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                                <input v-model="search"
+                                       type="search"
+                                       id="default-search"
+                                       class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       :placeholder="accounts.data !== undefined && accounts.data.length > 0 ? accounts.data[Math.floor(Math.random() * accounts.data.length)].username : 'No results'" />
                                 <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
 
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('/accounts')->group(function() {
+    Route::post('/search', [App\Http\Controllers\Api\AccountController::class, 'search'])->name('api.accounts.search');
+});
