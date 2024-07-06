24d66462520f8cbc349f97bb79e5cd77ee20dc77
Zlimon <simon@lonnestad.no>
2024-07-06 15:56:45 +0200

    Flowbite TextInput

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -21,7 +21,7 @@ defineExpose({ focus: () => input.value.focus() });
 <template>
     <input
         ref="input"
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
     >
