d9bf1c9031f5aaf9a2747fcbb9f361b815c2a6d1
Zlimon <simon@lonnestad.no>
2024-07-06 14:50:30 +0200

    Revert "Button components"
    This reverts commit fa9deec0511473bbb11de52d49f9617744f800e7.

--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -1,26 +1,14 @@
 <script setup>
-import { computed } from 'vue';
-
-const props = defineProps({
+defineProps({
     type: {
         type: String,
         default: 'button',
     },
-  disabled: Boolean,
-});
-
-const classes = computed(() => {
-    return props.disabled
-        ? 'opacity-50 cursor-not-allowed'
-        : '';
 });
 </script>
 
 <template>
-    <button :type="type"
-            class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
-            :class="classes"
-            :disabled="disabled">
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
         <slot />
     </button>
 </template>
