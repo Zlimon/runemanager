a1121d04207cb617f7d5a01af4d91661f5bd25b4
Zlimon <simon@lonnestad.no>
2024-07-07 13:31:45 +0200

    Seed Collection first

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,8 +34,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            AccountSeeder::class,
             CollectionSeeder::class,
+            AccountSeeder::class,
         ]);
 
         Artisan::call(
