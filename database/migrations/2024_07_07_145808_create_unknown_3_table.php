4a1c08f38e2d7c0fd2dd51d99b5cd80efe1dc871
Zlimon <simon@lonnestad.no>
2024-07-07 16:59:08 +0200

    Add missing Models and Migrations

---
+++ database/migrations/2024_07_07_145808_create_unknown_3_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unknown_3', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('unknown_3');
+    }
+};
