473a63d06330100f333db89377351be7ab0ad0fc
Zlimon <simon@lonnestad.no>
2024-07-18 00:15:48 +0200

    Setup mongodb database

--- config/database.php
+++ config/database.php
@@ -109,6 +109,12 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('MONGO_DB_URI'),
+            'database' => env('MONGO_DB_DATABASE', 'laravel'),
+        ],
+
     ],
 
     /*
