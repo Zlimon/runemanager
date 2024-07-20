329ebefa4729d8187814db28a07487daf359fd9b
Zlimon <simon@lonnestad.no>
2024-07-21 00:10:37 +0200

    Handle multiple (error) messages

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -1,12 +1,18 @@
 <script setup>
 defineProps({
     message: String,
+    messages: Array,
 });
 </script>
 
 <template>
-    <div v-show="message">
-        <p class="text-sm text-red-600">
+    <div v-if="message" v-show="message">
+        <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+            {{ message }}
+        </p>
+    </div>
+    <div v-else v-for="message in messages" v-show="messages">
+        <p class="mt-2 text-sm text-red-600 dark:text-red-500">
             {{ message }}
         </p>
     </div>
