da6fe1d65bca75dc3bc91f3bad4d76bbb3d40413
Zlimon <simon@lonnestad.no>
2024-07-22 17:12:20 +0200

    Move flowbite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,11 @@
 import './bootstrap';
 import '../css/app.css';
+import 'flowbite';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import 'flowbite';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
