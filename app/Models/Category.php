27a77b6a1799870555009919e637407286635fbc
Zlimon <simon@lonnestad.no>
2024-07-26 22:04:26 +0200

    Category slug setter

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,34 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 /**
- * App\Category
+ *
  *
  * @property int $id
- * @property string $category
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Broadcast[] $broadcast
- * @property-read int|null $broadcast_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Collection[] $collection
- * @property-read int|null $collection_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Log[] $log
- * @property-read int|null $log_count
+ * @property-read string $category
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Collection> $collections
+ * @property-read int|null $collections_count
  * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Category query()
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereCategory($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Category whereId($value)
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Collection> $collections
- * @property-read int|null $collections_count
  * @mixin \Eloquent
  */
 class Category extends Model
 {
+    protected function slug(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => Str::slug($value),
+        );
+    }
+
     public function collections(): HasMany
     {
-        return $this->hasMany(Collection::class, 'category_id');
+        return $this->hasMany(Collection::class);
     }
 
 //    public function broadcast()
