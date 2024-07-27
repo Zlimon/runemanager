29ee26d61649e41c2a04f30b67af93eb8c41929f
Zlimon <simon@lonnestad.no>
2024-07-27 11:03:04 +0200

    Support items when creating models

--- app/Traits/CollectionTrait.php
+++ app/Traits/CollectionTrait.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use App\Models\Category;
 use App\Models\Collection;
+use App\Models\Item;
 use Exception;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
@@ -14,13 +15,14 @@ trait CollectionTrait
     /**
      * @param Category $category
      * @param string $name
+     * @param array $items
      * @return Collection
      * @throws Exception
      */
-    public function createHiscore(Category $category, string $name): Collection
+    public function createHiscore(Category $category, string $name, array $items = []): Collection
     {
         try {
-            $this->createModel($category, $name);
+            $this->createModel($category, $name, $items);
         } catch (Exception $e) {
             throw $e;
         }
@@ -43,10 +45,11 @@ trait CollectionTrait
     /**
      * @param Category $category
      * @param string $name
+     * @param array<Item> $items
      * @return void
      * @throws Exception
      */
-    public function createModel(Category $category, string $name): void
+    public function createModel(Category $category, string $name, array $items = []): void
     {
         $modelName = $this->formatModelName($name);
 
@@ -56,9 +59,50 @@ trait CollectionTrait
 
         try {
             $model = sprintf("%s/%s", Str::studly($category->slug), $modelName);
-            $makeModel = sprintf("make:model %s", $model);
 
-            Artisan::call($makeModel);
+                $modelPath = 'app/Models/' . $model . '.php';
+
+                $table = '$table';
+                $fillable = '$fillable';
+                $hidden = '$hidden';
+                $thisBelongsTo = '$this->belongsTo(\App\Account::class)';
+
+                $modelFile = <<<EOD
+                <?php
+
+                namespace App\Npc;
+
+                use Illuminate\Database\Eloquent\Model;
+
+                class $modelName extends Model
+                {
+                    protected $table = '$modelName';
+
+                    protected $fillable = [
+                        'obtained',
+                        'kill_count',\r\n
+                EOD;
+                foreach ($items as $unique) {
+                    dd($unique);
+                    $fillable = str_replace("'", "", str_replace("-", "_", Str::snake(strtolower($unique))));
+
+                    $modelFile .= <<<EOD
+                            '$fillable',\r\n
+                    EOD;
+                }
+                $modelFile .= <<<EOD
+                    ];
+
+                    protected $hidden = ['user_id'];
+
+                    public function account()
+                    {
+                        return $thisBelongsTo;
+                    }
+                }
+                EOD;
+
+            File::put($modelPath, $modelFile);
         } catch (Exception $e) {
             throw new Exception(sprintf("Could not create model: '%s'. Message: %s", $modelName, $e->getMessage()));
         }
