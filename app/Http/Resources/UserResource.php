761f80c4c622f8ef6fb03eda338e6abaf84168bb
Zlimon <simon@lonnestad.no>
2024-08-06 00:03:34 +0200

    Remove some attributes from UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,16 +16,16 @@ class UserResource extends JsonResource
     {
         return [
             'name' => $this->name,
-            'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at,
-            'password' => $this->password,
+//            'email' => $this->email,
+//            'email_verified_at' => $this->email_verified_at,
+//            'password' => $this->password,
             'icon_id' => $this->icon_id,
             'private' => $this->private,
             'current_team_id' => $this->current_team_id,
-            'two_factor_secret' => $this->two_factor_secret,
-            'two_factor_recovery_codes' => $this->two_factor_recovery_codes,
-            'two_factor_confirmed_at' => $this->two_factor_confirmed_at,
-            'remember_token' => $this->remember_token,
+//            'two_factor_secret' => $this->two_factor_secret,
+//            'two_factor_recovery_codes' => $this->two_factor_recovery_codes,
+//            'two_factor_confirmed_at' => $this->two_factor_confirmed_at,
+//            'remember_token' => $this->remember_token,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
