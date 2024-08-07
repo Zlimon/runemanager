3309802db30f2013fccba4867f7ae8a47052691a
Zlimon <simon@lonnestad.no>
2024-08-08 00:51:45 +0200

    Remove skip model check on create migration

--- app/Traits/CollectionTrait.php
+++ app/Traits/CollectionTrait.php
@@ -126,12 +126,11 @@ trait CollectionTrait
      * @param Category $category
      * @param string $name
      * @param array $items
-     * @param bool $skipModelCheck
      * @throws Exception
      */
-    public function createMigration(Category $category, string $name, array $items = [], bool $skipModelCheck = false): void
+    public function createMigration(Category $category, string $name, array $items = []): void
     {
-        if (!$skipModelCheck && !class_exists(sprintf("app\Models\%s\%s", Str::studly($category->slug), $this->formatModelName($name)))) {
+        if (!class_exists(sprintf("app\Models\%s\%s", Str::studly($category->slug), $this->formatModelName($name)))) {
             throw new Exception(sprintf("Could not create migration: Model '%s' does not exist.", $this->formatModelName($name)));
         }
 
