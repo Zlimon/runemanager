7cfd868f03b7b7bf62ebafece51b82ebc659c362
Zlimon <simon@lonnestad.no>
2024-08-09 20:55:07 +0200

    Add HiscoreHelper & Remove manual merging in CollectionSeeder

---
+++ app/Helpers/HiscoreHelper.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Support\Str;
+
+/**
+ * Categories:
+ * 1 - skill
+ * 2 - pvp
+ * 3 - clue
+ * 4 - minigame
+ * 5 - boss
+ * 6 - raid
+ * 7 - npc
+ */
+class HiscoreHelper
+{
+    public static function all(): array
+    {
+        return [
+            'skill' => self::skill(),
+            'pvp' => self::pvp(),
+            'clue' => self::clue(),
+            'minigame' => self::minigame(),
+            'boss' => self::boss(),
+        ];
+    }
+
+    public static function skill(): array
+    {
+        return [
+            Str::slug('Overall') => 'Overall',
+            Str::slug('Attack') => 'Attack',
+            Str::slug('Defence') => 'Defence',
+            Str::slug('Strength') => 'Strength',
+            Str::slug('Hitpoints') => 'Hitpoints',
+            Str::slug('Ranged') => 'Ranged',
+            Str::slug('Prayer') => 'Prayer',
+            Str::slug('Magic') => 'Magic',
+            Str::slug('Cooking') => 'Cooking',
+            Str::slug('Woodcutting') => 'Woodcutting',
+            Str::slug('Fletching') => 'Fletching',
+            Str::slug('Fishing') => 'Fishing',
+            Str::slug('Firemaking') => 'Firemaking',
+            Str::slug('Crafting') => 'Crafting',
+            Str::slug('Smithing') => 'Smithing',
+            Str::slug('Mining') => 'Mining',
+            Str::slug('Herblore') => 'Herblore',
+            Str::slug('Agility') => 'Agility',
+            Str::slug('Thieving') => 'Thieving',
+            Str::slug('Slayer') => 'Slayer',
+            Str::slug('Farming') => 'Farming',
+            Str::slug('Runecrafting') => 'Runecrafting',
+            Str::slug('Hunter') => 'Hunter',
+            Str::slug('Construction') => 'Construction',
+        ];
+    }
+
+    public static function pvp(): array
+    {
+        return [
+            Str::slug('Bounty Hunter - Hunter') => 'Bounty Hunter - Hunter',
+            Str::slug('Bounty Hunter - Rogue') => 'Bounty Hunter - Rogue',
+            Str::slug('Bounty Hunter (Legacy) - Hunter') => 'Bounty Hunter (Legacy) - Hunter',
+            Str::slug('Bounty Hunter (Legacy) - Rogue') => 'Bounty Hunter (Legacy) - Rogue',
+        ];
+    }
+
+    public static function clue(): array
+    {
+        return [
+            Str::slug('Clue Scrolls (all)') => 'Clue Scrolls (all)',
+            Str::slug('Beginner Treasure Trails') => 'Beginner Treasure Trails',
+            Str::slug('Easy Treasure Trails') => 'Easy Treasure Trails',
+            Str::slug('Medium Treasure Trails') => 'Medium Treasure Trails',
+            Str::slug('Hard Treasure Trails') => 'Hard Treasure Trails',
+            Str::slug('Elite Treasure Trails') => 'Elite Treasure Trails',
+            Str::slug('Master Treasure Trails') => 'Master Treasure Trails',
+        ];
+    }
+
+    public static function minigame(): array
+    {
+        return [
+            Str::slug('LMS - Rank') => 'LMS - Rank',
+            Str::slug('PvP Arena - Rank') => 'PvP Arena - Rank',
+            Str::slug('Soul Wars Zeal') => 'Soul Wars Zeal',
+            Str::slug('Rifts closed') => 'Rifts closed',
+            Str::slug('Colosseum Glory') => 'Colosseum Glory',
+        ];
+    }
+
+    public static function boss(): array
+    {
+        return [
+            Str::slug('Abyssal Sire') => 'Abyssal Sire',
+            Str::slug('Alchemical Hydra') => 'Alchemical Hydra',
+            Str::slug('Artio') => 'Artio',
+            Str::slug('Barrows Chests') => 'Barrows Chests',
+            Str::slug('Bryophyta') => 'Bryophyta',
+            Str::slug('Callisto') => 'Callisto',
+            Str::slug("Calvar'ion") => "Calvar'ion",
+            Str::slug('Cerberus') => 'Cerberus',
+//            Str::slug('Chambers of Xeric') => 'Chambers of Xeric',
+//            Str::slug('Chambers of Xeric: Challenge Mode') => 'Chambers of Xeric: Challenge Mode',
+            Str::slug('Chaos Elemental') => 'Chaos Elemental',
+            Str::slug('Chaos Fanatic') => 'Chaos Fanatic',
+            Str::slug('Commander Zilyana') => 'Commander Zilyana',
+            Str::slug('Corporeal Beast') => 'Corporeal Beast',
+            Str::slug('Crazy Archaeologist') => 'Crazy Archaeologist',
+            Str::slug('Dagannoth Prime') => 'Dagannoth Prime',
+            Str::slug('Dagannoth Rex') => 'Dagannoth Rex',
+            Str::slug('Dagannoth Supreme') => 'Dagannoth Supreme',
+            Str::slug('Deranged Archaeologist') => 'Deranged Archaeologist',
+            Str::slug('Duke Sucellus') => 'Duke Sucellus',
+            Str::slug('General Graardor') => 'General Graardor',
+            Str::slug('Giant Mole') => 'Giant Mole',
+            Str::slug('Grotesque Guardians') => 'Grotesque Guardians',
+            Str::slug('Hespori') => 'Hespori',
+            Str::slug('Kalphite Queen') => 'Kalphite Queen',
+            Str::slug('King Black Dragon') => 'King Black Dragon',
+            Str::slug('Kraken') => 'Kraken',
+            Str::slug("Kree'Arra") => "Kree'Arra",
+            Str::slug("K'ril Tsutsaroth") => "K'ril Tsutsaroth",
+            Str::slug('Lunar Chests') => 'Lunar Chests',
+            Str::slug('Mimic') => 'Mimic',
+            Str::slug('Nex') => 'Nex',
+            Str::slug('Nightmare') => 'Nightmare',
+            Str::slug("Phosani's Nightmare") => "Phosani's Nightmare",
+            Str::slug('Obor') => 'Obor',
+            Str::slug('Phantom Muspah') => 'Phantom Muspah',
+            Str::slug('Sarachnis') => 'Sarachnis',
+            Str::slug('Scorpia') => 'Scorpia',
+            Str::slug('Scurrius') => 'Scurrius',
+            Str::slug('Skotizo') => 'Skotizo',
+            Str::slug('Sol Heredit') => 'Sol Heredit',
+            Str::slug('Spindel') => 'Spindel',
+            Str::slug('Tempoross') => 'Tempoross',
+            Str::slug('The Gauntlet') => 'The Gauntlet',
+            Str::slug('The Corrupted Gauntlet') => 'The Corrupted Gauntlet',
+            Str::slug('The Leviathan') => 'The Leviathan',
+            Str::slug('The Whisperer') => 'The Whisperer',
+//            Str::slug('Theatre of Blood') => 'Theatre of Blood',
+//            Str::slug('Theatre of Blood: Hard Mode') => 'Theatre of Blood: Hard Mode',
+            Str::slug('Thermonuclear Smoke Devil') => 'Thermonuclear Smoke Devil',
+//            Str::slug('Tombs of Amascut') => 'Tombs of Amascut',
+//            Str::slug('Tombs of Amascut: Expert Mode') => 'Tombs of Amascut: Expert Mode',
+            Str::slug('TzKal-Zuk') => 'TzKal-Zuk',
+            Str::slug('TzTok-Jad') => 'TzTok-Jad',
+            Str::slug('Vardorvis') => 'Vardorvis',
+            Str::slug('Venenatis') => 'Venenatis',
+            Str::slug("Vet'ion") => "Vet'ion",
+            Str::slug('Vorkath') => 'Vorkath',
+            Str::slug('Wintertodt') => 'Wintertodt',
+            Str::slug('Zalcano') => 'Zalcano',
+            Str::slug('Zulrah') => 'Zulrah',
+        ];
+    }
+}
--- database/seeders/CollectionSeeder.php
+++ database/seeders/CollectionSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Clients\CollectionLogClient;
+use App\Helpers\HiscoreHelper;
 use App\Models\Category;
 use App\Models\Item;
 use App\Traits\CollectionTrait;
@@ -13,114 +14,6 @@ use Illuminate\Support\Str;
 class CollectionSeeder extends Seeder
 {
     use CollectionTrait;
-
-    /**
-     * Categories:
-     * 1 - skill
-     * 2 - pvp
-     * 3 - clue
-     * 4 - minigame
-     * 5 - boss
-     * 6 - raid
-     * 7 - npc
-     */
-    private array $hiscoreEntries = [
-        'pvp' => [
-            'bounty-hunter' => 'Bounty Hunter - Hunter',
-            'bounty-hunter-rogue' => 'Bounty Hunter - Rogue',
-            'bounty-hunter-legacy' => 'Bounty Hunter (Legacy) - Hunter',
-            'bounty-hunter-rogue-legacy' => 'Bounty Hunter (Legacy) - Rogue',
-//            'unknown-1' => 'Unknown 1',
-//            'unknown-2' => 'Unknown 2',
-        ],
-        'clue' => [
-            'all-treasure-trails' => 'All Treasure Trails',
-            'beginner-treasure-trails' => 'Beginner Treasure Trails',
-            'easy-treasure-trails' => 'Easy Treasure Trails',
-            'medium-treasure-trails' => 'Medium Treasure Trails',
-            'hard-treasure-trails' => 'Hard Treasure Trails',
-            'elite-treasure-trails' => 'Elite Treasure Trails',
-            'master-treasure-trails' => 'Master Treasure Trails',
-        ],
-        'minigame' => [
-            'last-man-standing' => 'Last Man Standing',
-            'pvp-arena' => 'PvP Arena',
-            'soul-wars-zeal' => 'Soul Wars Zeal',
-            'rifts-closed' => 'Rifts closed',
-            'colosseum-glory' => 'Colosseum Glory',
-        ],
-        'boss' => [
-            'abyssal-sire' => 'Abyssal Sire',
-            'alchemical-hydra' => 'Alchemical Hydra',
-            'artio' => 'Artio',
-            'barrows-chests' => 'Barrows Chests',
-            'bryophyta' => 'Bryophyta',
-            'callisto' => 'Callisto',
-            'calvarion' => 'Calvar\'ion',
-            'cerberus' => 'Cerberus',
-//                'chambers-of-xeric' => 'Chambers of Xeric - Placeholder',
-//                'chambers-of-xeric-challenge-mode' => 'Chambers of Xeric: Challenge Mode - Placeholder',
-            'chaos-elemental' => 'Chaos Elemental',
-            'chaos-fanatic' => 'Chaos Fanatic',
-            'commander-zilyana' => 'Commander Zilyana',
-            'corporeal-beast' => 'Corporeal Beast',
-            'crazy-archaeologist' => 'Crazy Archaeologist',
-            'dagannoth-prime' => 'Dagannoth Prime',
-            'dagannoth-rex' => 'Dagannoth Rex',
-            'dagannoth-supreme' => 'Dagannoth Supreme',
-            'deranged-archaeologist' => 'Deranged Archaeologist',
-            'duke-sucellus' => 'Duke Sucellus',
-            'general-graardor' => 'General Graardor',
-            'giant-mole' => 'Giant Mole',
-            'grotesque-guardians' => 'Grotesque Guardians',
-            'hespori' => 'Hespori',
-            'kalphite-queen' => 'Kalphite Queen',
-            'king-black-dragon' => 'King Black Dragon',
-            'kraken' => 'Kraken',
-            'kreearra' => 'Kree\'arra',
-            'kril-tsutsaroth' => 'K\'ril Tsutsaroth',
-            'lunar-chests' => 'Lunar Chests',
-            'mimic' => 'Mimic',
-            'nex' => 'Nex',
-            'nightmare' => 'Nightmare',
-            'phosanis-nightmare' => 'Phosani\'s Nightmare',
-            'obor' => 'Obor',
-            'phantom-muspah' => 'Phantom Muspah',
-            'sarachnis' => 'Sarachnis',
-            'scorpia' => 'Scorpia',
-            'scurrius' => 'Scurrius',
-            'skotizo' => 'Skotizo',
-            'sol-heredit' => 'Sol Heredit',
-            'spindel' => 'Spindel',
-            'tempoross' => 'Tempoross',
-            'the-gauntlet' => 'The Gauntlet',
-            'the-corrupted-gauntlet' => 'The Corrupted Gauntlet',
-            'the-leviathan' => 'The Leviathan',
-            'the whisperer' => 'The Whisperer',
-//                'theatre-of-blood' => 'Theatre of Blood - Placeholder',
-//                'theatre-of-blood-hard-mode' => 'Theatre of Blood: Hard Mode - Placeholder',
-            'thermonuclear-smoke-devil' => 'Thermonuclear Smoke Devil',
-//                'tombs-of-amascut' => 'Tombs of Amascut - Placeholder',
-//                'tombs-of-amascut-expert-mode' => 'Tombs of Amascut: Expert Mode - Placeholder',
-            'tzkal-zuk' => 'TzKal-Zuk',
-            'tztok-jad' => 'TzTok-Jad',
-            'vardorvis' => 'Vardorvis',
-            'venenatis' => 'Venenatis',
-            'vetion' => 'Vet\'ion',
-            'vorkath' => 'Vorkath',
-            'wintertodt' => 'Wintertodt',
-            'zalcano' => 'Zalcano',
-            'zulrah' => 'Zulrah',
-        ],
-        'raid' => [
-            'chambers-of-xeric' => 'Chambers of Xeric',
-            'chambers-of-xeric-challenge-mode' => 'Chambers of Xeric: Challenge Mode',
-            'theatre-of-blood' => 'Theatre of Blood',
-            'theatre-of-blood-hard-mode' => 'Theatre of Blood: Hard Mode',
-            'tombs-of-amascut' => 'Tombs of Amascut',
-            'tombs-of-amascut-expert-mode' => 'Tombs of Amascut: Expert Mode',
-        ]
-    ];
 
     /**
      * Run the database seeds.
@@ -180,6 +73,7 @@ class CollectionSeeder extends Seeder
             return;
         }
 
+        $hiscoreEntries = HiscoreHelper::all();
         $storeItems = false;
         foreach ($result['collectionLog']['tabs'] as $category => $hiscores) {
             // collectionLogTab is the collection name on collectionlog.net
@@ -194,94 +88,15 @@ class CollectionSeeder extends Seeder
 
             $category = Category::whereSlug($category)->first();
 
+            if (!$category) {
+                $this->command->warn(sprintf("Category '%s' does not exist.", $category));
+
+                continue;
+            }
+
             foreach ($hiscores as $name => $hiscore) {
-//                Merge certain collections
-//                switch ($name) {
-//                    case 'Callisto':
-//                    case 'Artio':
-//                        $collectionName = 'Callisto and Artio';
-//                        break;
-//                    case 'Vet\'ion':
-//                    case 'Calvar\'ion':
-//                        $collectionName = 'Vet\'ion and Calvar\'ion';
-//                        break;
-//                    case 'Dagannoth Prime':
-//                    case 'Dagannoth Rex':
-//                    case 'Dagannoth Supreme':
-//                        $collectionName = 'Dagannoth Kings';
-//                        break;
-//                    case 'Deranged Archaeologist':
-//                        break;
-//                    case 'Lunar Chests':
-//                        $collectionName = 'Moons of Peril';
-//                        break;
-//                    case 'Mimic':
-//                        break;
-//                    case 'Nightmare':
-//                    case 'Phosani\'s Nightmare':
-//                        $collectionName = 'The Nightmare';
-//                        break;
-//                    case 'Spindel':
-//                    case 'Venenatis':
-//                        $collectionName = 'Venenatis and Spindel';
-//                        break;
-//                    case 'Gauntlet':
-//                    case 'The Corrupted Gauntlet':
-//                        $collectionName = 'The Gauntlet';
-//                        break;
-//                    case 'TzKal-Zuk':
-//                        $collectionName = 'The Inferno';
-//                        break;
-//                    case 'TzTok-Jad':
-//                        $collectionName = 'The Fight Caves';
-//                        break;
-//                    default:
-//                        $collectionName = $name;
-//                        break;
-//                }
-
-                $slug = Str::slug($name);
-
-                switch ($name) {
-                    case 'Callisto and Artio':
-                        $this->hiscoreEntries[$category->slug][Str::slug('Callisto')] = 'Callisto';
-                        $this->hiscoreEntries[$category->slug][Str::slug('Artio')] = 'Artio';
-                        break;
-                    case 'Vet\'ion and Calvar\'ion':
-                        $this->hiscoreEntries[$category->slug][Str::slug('Vet\'ion')] = 'Vet\'ion';
-                        $this->hiscoreEntries[$category->slug][Str::slug('Calvar\'ion')] = 'Calvar\'ion';
-                        break;
-                    case 'Dagannoth Kings':
-                        $this->hiscoreEntries[$category->slug][Str::slug('Dagannoth Prime')] = 'Dagannoth Prime';
-                        $this->hiscoreEntries[$category->slug][Str::slug('Dagannoth Rex')] = 'Dagannoth Rex';
-                        $this->hiscoreEntries[$category->slug][Str::slug('Dagannoth Supreme')] = 'Dagannoth Supreme';
-                        break;
-                    case 'Moons of Peril':
-                        $this->hiscoreEntries[$category->slug][Str::slug('Lunar Chests')] = 'Lunar Chests';
-                        break;
-                    case 'The Nightmare':
-                        $this->hiscoreEntries[$category->slug][Str::slug('Nightmare')] = 'Nightmare';
-                        $this->hiscoreEntries[$category->slug][Str::slug('Phosani\'s Nightmare')] = 'Phosani\'s Nightmare';
-                        break;
-                    case 'Venenatis and Spindel':
-                        $this->hiscoreEntries[$category->slug][Str::slug('Spindel')] = 'Spindel';
-                        $this->hiscoreEntries[$category->slug][Str::slug('Venenatis')] = 'Venenatis';
-                        break;
-                    case 'The Gauntlet':
-                        $this->hiscoreEntries[$category->slug][Str::slug('Gauntlet')] = 'Gauntlet';
-                        $this->hiscoreEntries[$category->slug][Str::slug('The Corrupted Gauntlet')] = 'The Corrupted Gauntlet';
-                        break;
-                    case 'The Inferno':
-                        $this->hiscoreEntries[$category->slug][Str::slug('TzKal-Zuk')] = 'TzKal-Zuk';
-                        break;
-                    case 'The Fight Caves':
-                        $this->hiscoreEntries[$category->slug][Str::slug('TzTok-Jad')] = 'TzTok-Jad';
-                        break;
-                    default:
-                        // Unset entries already fetched from collectionlog.net as we only want to create hiscore entries unique to OSRS hiscores
-                        unset($this->hiscoreEntries[$category->slug][Str::slug($name)]);
-                        break;
-                }
+                // Unset entries already fetched from collectionlog.net as we only want to create hiscore entries unique to OSRS hiscores
+                unset($hiscoreEntries[$category->slug][Str::slug($name)]);
 
                 $items = [];
                 if ($storeItems && !empty($hiscore['items'])) {
@@ -312,20 +127,18 @@ class CollectionSeeder extends Seeder
 
                     continue;
                 }
-
-//                try {
-//                    $collection = $this->getOrCreateCollection($category, $name, true);
-//                } catch (Exception $e) {
-//                    $this->command->warn($e->getMessage());
-//
-//                    continue;
-//                }
             }
         }
 
-        // Create separate collections for hiscore entries not merged in collectionlog.net
-        foreach ($this->hiscoreEntries as $category => $hiscores) {
+        // Create separate collections for hiscore entries not used in collectionlog.net
+        foreach ($hiscoreEntries as $category => $hiscores) {
             $category = Category::whereSlug($category)->first();
+
+            if (!$category) {
+                $this->command->warn(sprintf("Category '%s' does not exist.", $category));
+
+                continue;
+            }
 
             foreach ($hiscores as $name) {
                 try {
