24517c5d30ce444c494064a0a8c8b5141e34edd5
Zlimon <simon@lonnestad.no>
2024-08-09 11:16:19 +0200

    Add generated models and migrations (remaining hiscoreentries)

---
+++ app/Models/Boss/DerangedArchaeologist.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Boss;
+
+use App\Models\Account;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class DerangedArchaeologist extends Model
+{
+    protected $table = 'deranged_archaeologist';
+
+    protected $fillable = [
+        'obtained',
+        'kill_count',
+    ];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+}
---
+++ database/migrations/2024_08_09_091528_create_pvp_arena_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePvpArenaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('pvp_arena', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->integer('obtained')->default(0);
+            $table->integer('kill_count')->default(0);
+            $table->timestamps();
+
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pvp_arena');
+    }
+};
