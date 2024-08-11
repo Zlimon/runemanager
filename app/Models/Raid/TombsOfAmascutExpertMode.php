9abf374ff7040527735149a3315df3cfd3e60b21
Zlimon <simon@lonnestad.no>
2024-08-11 21:51:47 +0200

    Add rank column to Collection migrations & Add additional raid models

---
+++ app/Models/Raid/TombsOfAmascutExpertMode.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Raid;
+
+use App\Models\Account;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TombsOfAmascutExpertMode extends Model
+{
+    protected $table = 'tombs_of_amascut_expert_mode';
+
+    protected $fillable = [
+        'obtained',
+        'kill_count',
+    ];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+}
---
+++ database/migrations/2024_08_11_182730_create_shooting_stars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShootingStarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('shooting_stars', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->integer('rank')->default(0);
+            $table->integer('kill_count')->default(0);
+            $table->integer('obtained')->default(0);
+            $table->timestamps();
+
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shooting_stars');
+    }
+};
