a8116d3d21830f20ae6f454af9111b130f82e5bf
Zlimon <simon@lonnestad.no>
2024-08-29 19:23:48 +0200

    Add ironman suffix to account type enums & Support flipping of enum array values

--- app/Enums/AccountTypesEnum.php
+++ app/Enums/AccountTypesEnum.php
@@ -5,15 +5,17 @@ namespace App\Enums;
 enum AccountTypesEnum: string {
     case NORMAL  = 'normal';
     case IRONMAN = 'ironman';
-    case HARDCORE_IRONMAN = 'hardcore';
-    case ULTIMATE_IRONMAN = 'ultimate';
+    case HARDCORE_IRONMAN = 'hardcore_ironman';
+    case ULTIMATE_IRONMAN = 'ultimate_ironman';
 
-    public static function returnAllAccountTypes(): array {
-        return [
+    public static function returnAllAccountTypes($flip = false): array {
+        $allAccountTypes = [
             AccountTypesEnum::NORMAL->name => AccountTypesEnum::NORMAL->value,
             AccountTypesEnum::IRONMAN->name => AccountTypesEnum::IRONMAN->value,
             AccountTypesEnum::HARDCORE_IRONMAN->name => AccountTypesEnum::HARDCORE_IRONMAN->value,
             AccountTypesEnum::ULTIMATE_IRONMAN->name => AccountTypesEnum::ULTIMATE_IRONMAN->value,
         ];
+
+        return $flip ? array_flip($allAccountTypes) : $allAccountTypes;
     }
 }
