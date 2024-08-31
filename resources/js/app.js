d10548150c362c20b6063dffd0ddb8b79c5e3857
Zlimon <simon@lonnestad.no>
2024-08-31 02:30:19 +0200

    Install dayjs package

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import dayjs from 'dayjs';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,10 +14,13 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
+        const app = createApp({ render: () => h(App, props) });
+
+        app.config.globalProperties.$dayjs = dayjs;
+
+        app.use(plugin)
+           .use(ZiggyVue)
+           .mount(el);
     },
     progress: {
         color: '#4B5563',
