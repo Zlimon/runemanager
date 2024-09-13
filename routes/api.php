b34fd9f988130ef4f60cacca4a2b2b167b32f0b6
Zlimon <simon@lonnestad.no>
2024-09-13 18:22:38 +0200

    Move API routes within auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,28 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/npc/search', [App\Http\Controllers\Api\NpcController::class, 'search'])->name('api.npc.search');
+Route::middleware([
+    'auth:sanctum',
+])->group(function () {
+    Route::get('/me', [App\Http\Controllers\Api\AuthController::class, 'me']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::prefix('/accounts')->group(function() {
+        Route::post('/search', [App\Http\Controllers\Api\AccountController::class, 'search'])->name('api.accounts.search');
+        Route::put('/{account}/update', [App\Http\Controllers\Api\AccountController::class, 'update'])->name('api.accounts.update');
+    });
 
-Route::prefix('/accounts')->group(function() {
-    Route::post('/search', [App\Http\Controllers\Api\AccountController::class, 'search'])->name('api.accounts.search');
-});
+    Route::post('/npc/search', [App\Http\Controllers\Api\NpcController::class, 'search'])->name('api.npc.search');
 
-Route::prefix('/hiscores')->group(function() {
-    Route::post('/store', [App\Http\Controllers\Admin\Api\HiscoreController::class, 'store'])->name('admin.api.hiscores.store');
-});
-
-Route::prefix('/collectionlog')->group(function() {
     Route::prefix('/collectionlog')->group(function() {
         Route::prefix('/user')->group(function() {
-            Route::post('/', [App\Http\Controllers\Api\CollectionLogController::class, 'user'])->name('api.collectionlog.collectionlog.user');
+            Route::post('/', [App\Http\Controllers\Api\CollectionLogController::class, 'user'])->name('api.collectionlog.user');
+        });
+    });
+
+    Route::prefix('/admin')->group(function() {
+        Route::prefix('/hiscores')->group(function() {
+            Route::post('/store', [App\Http\Controllers\Admin\Api\HiscoreController::class, 'store'])->name('admin.api.hiscores.store');
         });
     });
 });
