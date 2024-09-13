3124f3ae4203ba7d44161e6be7184b27b3cedf51
Zlimon <simon@lonnestad.no>
2024-09-13 18:21:07 +0200

    Enable statefulApi middleware for authenticated Sanctum Axios requests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
+        $middleware->statefulApi();
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
