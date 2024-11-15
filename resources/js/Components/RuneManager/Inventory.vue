e11d72020c1e165c55fc835b392a5410da29d7a9
Zlimon <simon@lonnestad.no>
2024-11-15 23:47:40 +0100

    Use _id as key & Check if item.item is set

--- resources/js/Components/RuneManager/Inventory.vue
+++ resources/js/Components/RuneManager/Inventory.vue
@@ -33,30 +33,34 @@ const getInventory = () => {
         <div v-if="inventory !== null">
             <ul class="m-2 grid grid-cols-4 gap-2">
                 <li v-for="(item, slot) in inventory.inventory" class="flex items-center justify-between">
-                    <button :data-tooltip-target="`inventory-${slot}-${item.item.id}-tooltip-bottom`"
-                            data-tooltip-placement="bottom"
-                            type="button"
-                            class="relative h-20 w-20 rounded-lg border p-4 border-beige-700 dark:border-gray-700"
-                            :class="{'cursor-default': item.item.id === -1}">
-                        <span v-if="item.quantity > 1"
-                              class="absolute top-0 left-0 p-1 text-sm">
-                            {{ item.quantity }}
-                        </span>
-                        <img v-if="item.item.icon"
-                             :src="`data:image/jpeg;base64,${item.item.icon}`"
-                             class="mx-auto h-10 w-10 object-contain"
-                             :class="{ 'opacity-50': item.quantity === 0 }"
-                             loading="lazy"
-                             @error="handleImageError">
-                        <span v-else>
-                            {{ item.item.name }}
-                        </span>
-                    </button>
+                    <div class="relative h-20 w-20 rounded-lg border p-4 border-beige-700 dark:border-gray-700">
+                        <button v-if="item.item !== null"
+                                :data-tooltip-target="`inventory-${slot}-${item._id}-tooltip-bottom`"
+                                data-tooltip-placement="bottom"
+                                type="button"
+                                class=""
+                                :class="{'cursor-default': item._id === -1}">
+                            <span v-if="item.quantity > 1"
+                                  class="absolute top-0 left-0 p-1 text-sm">
+                                {{ item.quantity }}
+                            </span>
+                            <img v-if="item.item?.icon"
+                                 :src="`data:image/jpeg;base64,${item.item.icon}`"
+                                 class="mx-auto h-10 w-10 object-contain"
+                                 :class="{ 'opacity-50': item.quantity === 0 }"
+                                 loading="lazy"
+                                 @error="handleImageError">
+                            <span v-else>
+                                {{ item.item?.name }}
+                            </span>
+                        </button>
+                    </div>
 
-                    <div :id="`inventory-${slot}-${item.item.id}-tooltip-bottom`"
+                    <div v-if="item.item !== null"
+                         :id="`inventory-${slot}-${item.item._id}-tooltip-bottom`"
                          role="tooltip"
                          class="invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm tooltip dark:bg-gray-700"
-                         :class="{'hidden': item.item.id === -1}">
+                         :class="{'hidden': item.item._id === -1}">
                         <p>
                             {{ item.item.name }}
                         </p>
