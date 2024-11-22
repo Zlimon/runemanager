c5fbc16632eb5c586c6a8d1555059402660092ea
Zlimon <simon@lonnestad.no>
2024-11-22 23:31:56 +0100

    Add daisyui to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from "daisyui"
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -23,9 +24,11 @@ export default {
                     100: '#F5F1E7',
                     200: '#EDE7D5',
                     300: '#E2D9BF',
+
                     400: '#D8CCB5',
                     500: '#C0B29E',
                     600: '#A79886',
+
                     700: '#8F7F6E',
                     800: '#766556',
                     900: '#5E4C3E',
@@ -34,5 +37,36 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [forms, typography, daisyui],
+
+    daisyui: {
+        themes: [
+            // "retro",
+            // "dark",
+            {
+                runemanager: {
+                    "primary": "#EF9995",
+                    "primary-content": "#282425",
+                    "secondary": "#A4CBB4",
+                    "secondary-content": "#282425",
+                    "accent": "#DC8850",
+                    "accent-content": "#282425",
+                    "neutral": "#2E282A",
+                    "neutral-content": "#EDE6D4",
+                    "base-100": "#d8ccb4",
+                    "base-200": "#E2D9BF",
+                    "base-300": "#94866D",
+                    "base-content": "#282425",
+                    "info": "#2563EB",
+                    "info-content": "#D2E2FF",
+                    "success": "#16A34A",
+                    "success-content": "#000A02",
+                    "warning": "#D97706",
+                    "warning-content": "#110500",
+                    "error": "#F35248",
+                    "error-content": "#140202"
+                },
+            },
+        ],
+    },
 };
