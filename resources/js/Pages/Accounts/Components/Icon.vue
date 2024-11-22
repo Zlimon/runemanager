5e56963436cf2869f586af4290e871e34e6d9033
Zlimon <simon@lonnestad.no>
2024-11-23 00:28:32 +0100

    Display online indicator if online

--- resources/js/Pages/Accounts/Components/Icon.vue
+++ resources/js/Pages/Accounts/Components/Icon.vue
@@ -9,6 +9,10 @@ let account = ref(props.accountProp);
 </script>
 
 <template>
-    <img :src="`data:image/jpeg;base64,${account.icon}`"
-         class="h-16 w-16 rounded-full p-2 ring-2 ring-base-300">
+    <div class="indicator">
+        <span v-show="account.online === 1"
+              class="indicator-item badge badge-secondary"/>
+        <img :src="`data:image/jpeg;base64,${account.icon}`"
+             class="h-16 w-16 rounded-full p-2 ring-2 ring-base-300">
+    </div>
 </template>
