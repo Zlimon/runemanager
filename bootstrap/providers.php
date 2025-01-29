d1aa1f5dda5824b45eb3bdae3488eef2571d5071
Zlimon <simon@lonnestad.no>
2025-01-29 22:43:50 +0100

    Remove all

--- bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
-    MongoDB\Laravel\MongoDBServiceProvider::class,
-];
